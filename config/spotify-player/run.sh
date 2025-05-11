7adb98219c24e8c86839137dade384625a299700
Jaan Lavaerts <jaan.lavaerts2002@gmail.com>
2025-05-11 20:34:18 +0200

    kitty for better spotify_player support

--- config/spotify-player/run.sh
+++ config/spotify-player/run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-SESSION_NAME="spotify"
-WINDOW_NAME="spotify"
+WINDOW_TITLE="spotify"
+CMD="spotify_player"
 
-tmux has-session -t "$SESSION_NAME" 2>/dev/null
-
-if [ $? != 0 ]; then
-  tmux new-session -s "$SESSION_NAME" -n "$WINDOW_NAME" -d 'spotify_player'
-
-  tmux rename-window -t "${SESSION_NAME}:0" "$WINDOW_NAME"
-fi
-
-tmux attach-session -t "$SESSION_NAME"
+open -na kitty --args --title "$WINDOW_TITLE" sh -c "$CMD"
