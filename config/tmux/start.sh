097960e080c9f131fc52ee010322550e72f7946a
Jaan Lavaerts <jaan.lavaerts2002@gmail.com>
2025-05-17 10:07:22 +0200

    tmux start script

--- config/tmux/start.sh
+++ config/tmux/start.sh
@@ -2,12 +2,12 @@
 
 if ! tmux has-session -t default 2>/dev/null; then
     echo "Creating default session..."
-    tmux new-session -d -s default
+    /opt/homebrew/bin/tmux new-session -d -s default
 fi
 
 if ! tmux has-session -t spotify 2>/dev/null; then
     echo "Creating spotify session..."
-    tmux new-session -d -s spotify spotify_player
+    /opt/homebrew/bin/tmux new-session -d -s spotify /opt/homebrew/bin/spotify_player
 fi
 
-tmux attach -t default
+/opt/homebrew/bin/tmux attach-session -t default
